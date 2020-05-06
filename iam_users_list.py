525f3bf01761f2ef32d7428e7c863cd2041a77e7
kanuka prashanth <53951743+kanuka33@users.noreply.github.com>
2020-05-06 17:55:28 +0530

    Update iam_users_list.py

--- iam_users_list.py
+++ iam_users_list.py
@@ -13,4 +13,24 @@ for user in iam_con.list_users()['Users']:
         )
     )
     
- 
+
+    
+<================================================================================================================>    
+    
+    
+    
+    
+#Listing all IAM users within orginization
+import boto3
+aws_man=boto3.session.Session(profile_name="root")
+iam=aws_man.client(service_name="iam",region_name="us-east-1")
+paginator = iam.get_paginator('list_users')
+for page in paginator.paginate():
+    for user in page['Users']:
+        print("User: {0}\nUserID: {1}\nARN: {2}\nCreatedOn: {3}\n".format(
+        user['UserName'],
+        user['UserId'],
+        user['Arn'],
+        user['CreateDate']
+        )
+    )
